76e018ac4988e1f5ca83a0d00b61c87a846c8cf0
will rost <62968380+iamwillrost@users.noreply.github.com>
2022-01-29 22:03:36 -0600

    created generator

---
+++ assets/js/generator.js
@@ -0,0 +1,59 @@
+const CHRrangeEl = document.getElementById('CHRrange')
+const CHRnumberEl = document.getElementById('CHRnumber')
+
+const includeUpperEl = document.getElementById('includeUpper')
+const includeNumberEl = document.getElementById('includeNumber')
+const includeSymbolEl = document.getElementById('includeSymbol')
+const formEl = document.getElementById('passwordGenerate')
+
+const passwordDisplayEl = document.getElementById('passwordDisplay')
+
+const UPPERCASE_CharCode = arrayHighLow(65, 90)
+const LOWERCASE_CharCode = arrayHighLow(97,122)
+const NUMBER_CharCode = arrayHighLow(48,57)
+const Symbol_CharCode = arrayHighLow(33,47).concat(arrayHighLow(58, 64)).concat(arrayHighLow(91,96)).concat(arrayHighLow(123,126))
+
+CHRnumberEl.addEventListener('input', syncCHRAmount)
+CHRrangeEl.addEventListener('input', syncCHRAmount)
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault()
+    const CHRamountEl = CHRnumberEl.value
+    const UpperEl =includeUpperEl.checked
+    const NumberEl =includeNumberEl.checked
+    const SymbolEl =includeSymbolEl.checked
+    const password = generatePassword(CHRamountEl, UpperEl, NumberEl, SymbolEl)
+    passwordDisplayEl.innerText = password
+    console.log("new password is " + password)
+})
+
+function generatePassword(CHRamountEl, UpperEl, NumberEl, SymbolEl) {
+    let charcodes = LOWERCASE_CharCode
+    if (UpperEl) charcodes = charcodes.concat(UPPERCASE_CharCode)
+    if (NumberEl) charcodes = charcodes.concat(NUMBER_CharCode)
+    if (SymbolEl) charcodes = charcodes.concat(Symbol_CharCode)
+    console.log(UPPERCASE_CharCode)
+    console.log(LOWERCASE_CharCode) 
+    
+    const passwordCHRs = []
+    for(let i = 0; i < CHRamountEl; i++) {
+    const character = charcodes[Math.floor(Math.random() * charcodes.length)]
+        passwordCHRs.push(String.fromCharCode(character))
+        console.log(passwordCHRs)
+    }
+    return passwordCHRs.join('')
+}
+
+function arrayHighLow(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array
+}
+   
+function syncCHRAmount(e) {
+    const valueEl= e.target.value
+    CHRrangeEl.value = valueEl
+    CHRnumberEl.value = valueEl
+}
